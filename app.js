ba3e3b07540b15c046e098d8ebd5f0cf8e66f744
MayPyone <maypyone015@gmail.com>
2024-10-05 11:53:57 +0630

    Add api doc

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const app = express();
 const connection = require('./db/connect');
 const cors = require("cors");
 
+const swaggerUi = require('swagger-ui-express');
+const  swaggerJsdoc = require("swagger-jsdoc");
+ YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
 const authenticateUser = require('./middleware/authentication');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 const NotFoundError = require('./errors/not-found');
@@ -24,9 +29,43 @@ app.use('/api/v1/job',authenticateUser,jobRouter)
 
 app.use(errorHandlerMiddleware);
 
+// const options = {
+//   definition: {
+//     openapi: "3.1.0",
+//     info: {
+//       title: "LogRocket Express API with Swagger",
+//       version: "0.1.0",
+//       description:
+//         "This is a simple CRUD API application made with Express and documented with Swagger",
+//       license: {
+//         name: "MITb ",
+//         url: "https://spdx.org/licenses/MIT.html",
+//       },
+//       contact: {
+//         name: "LogRocket",
+//         url: "https://logrocket.com",
+//         email: "info@email.com",
+//       },
+//     },
+//     servers: [
+//       {
+//         url: "http://localhost:3000",
+//       },
+//     ],
+//   },
+//   apis: ["./routes/*.js"],
+// };
+
+// const specs = swaggerJsdoc(options);
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument)
+);
+
 const port = process.env.PORT || 3000
 
-const start = async () => { 
+const start = async () =>   { 
     try {
       app.listen(port, () =>
         console.log(`Server is listening on port ${port}...`)
